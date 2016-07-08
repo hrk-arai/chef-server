899c739b84db25be181d6346b431b4a0dcc6d70c
h-arai <h-arai@creationline.com>
2016-07-08 17:48:49 +0900

    modify mkdir option

--- install_chefserver.sh
+++ install_chefserver.sh
@@ -6,7 +6,7 @@ core="chef-server-core-12.1.2-1.el7.x86_64.rpm"
 reporting="opscode-reporting-1.5.6-1.el7.x86_64.rpm"
 manage="opscode-manage-1.21.0-1.el7.x86_64.rpm"
 
-mkdir $packages
+mkdir -p $packages
 
 # download packages
 curl -sL https://packages.chef.io/stable/el/$version/$core > $packages/$core
